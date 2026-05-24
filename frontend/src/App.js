39509b6f9ddb9656851056c7563fd200b2b4d4e8
SalviVishakha <vishakhasalvi603@gmail.com>
2026-05-24 20:34:23 +0530

    Fixed frontend repository issue

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5002")
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Multi-Container DevOps Project</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
